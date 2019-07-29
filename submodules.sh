2446ffc8a9ad2f7ea77c7a0aa58349714db469b2
Kota Mizushima <mizukota@gmail.com>
2019-07-29 10:26:14 +0900

    Shell script to add sobmodules (temporarily)

---
+++ submodules.sh
@@ -0,0 +1,25 @@
+git submodule add	git@github.com:nim-lang/Nim.git Nim 
+git submodule add	git@github.com:NexMirror/Octave.git Octave 
+git submodule add	git@github.com:microsoft/TypeScript.git TypeScript
+git submodule add	git@github.com:agda/agda.git agda 
+git submodule add	git@github.com:eclipse/ceylon.git ceylon 
+git submodule add	git@github.com:elm/compiler.git compiler 
+git submodule add	git@github.com:python/cpython.git cpython 
+git submodule add	git@github.com:elixir-lang/elixir.git elixir 
+git submodule add	git@github.com:fsharp/fsharp.git fsharp 
+git submodule add	git@github.com:apache/groovy.git groovy
+git submodule add	git@github.com:HaxeFoundation/haxe.git haxe 
+git submodule add	git@github.com:openjdk/jdk.git jdk 
+git submodule add	git@github.com:JuliaLang/julia.git julia 
+git submodule add	git@github.com:JetBrains/kotlin 
+git submodule add	git@github.com:llvm/llvm-project.git llvm-project 
+git submodule add	git@github.com:lua/lua.git lua 
+git submodule add	git@github.com:rsdn/nemerle.git nemerle 
+git submodule add	git@github.com:nodejs/node.git node 
+git submodule add	git@github.com:php/php-src.git php-src 
+git submodule add	git@github.com:facebook/reason.git reason 
+git submodule add	git@github.com:dotnet/roslyn.git roslyn 
+git submodule add	git@github.com:ruby/ruby.git ruby 
+git submodule add	git@github.com:scala/scala.git scala 
+git submodule add	git@github.com:xyproto/smlnj.git smlnj 
+git submodule add	git@github.com:wren-lang/wren wren
